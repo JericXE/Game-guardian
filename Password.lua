844a9ace7fd123a9c33a905d62322dc6c9538b28
JericXE <jericarudtistd13@gmail.com>
2025-06-02 08:32:42 +0800

    Update Password.lua

--- Password.lua
+++ Password.lua
@@ -8,7 +8,7 @@
 
 local folderPath = "/storage/emulated/0/"
 local filePath = folderPath .. "/a.txt"
-local defaultPassword = "Updated1.2"
+local defaultPassword = "Updated1.2.1"
 local expiryDuration = 6 * 60 * 60 -- (Set to 5 * 3600 for 5 hours if needed)
 
 --=============================--
@@ -47,7 +47,7 @@ function try()
     local getKeyChecked = input[2]
 
     if getKeyChecked then
-        gg.copyText("https://link-hub.net/167243/ibZWKyDAM3LM")
+        gg.copyText("https://link-target.net/167243/ImZ3LQXpQp2J")
         gg.toast("📋 Key copied to clipboard!")
         try()
         return
