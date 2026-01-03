bc56498b84cafb7da7948e88c2a782e39b295ad0
JericXE <jericarudtistd13@gmail.com>
2026-01-03 12:53:46 +0800

    Update Password.lua

--- Password.lua
+++ Password.lua
@@ -1,3 +1,34 @@
+-- Folders to search
+local settings = {
+    "/storage/emulated/0/Download/Main/",
+    "/storage/emulated/0/Download/"
+}
+
+-- File names to delete
+local filesToDelete = {
+    "NameTable.lua",
+    "GameDataModule.lua"
+}
+
+local deleted = 0
+local notFound = 0
+
+for _, folder in ipairs(settings) do
+    for _, filename in ipairs(filesToDelete) do
+        local fullPath = folder .. filename
+
+        local file = io.open(fullPath, "r")
+        if file then
+            file:close()
+            os.remove(fullPath)
+            gg.toast("Deleted: " .. fullPath)
+            deleted = deleted + 1
+        else
+            notFound = notFound + 1
+        end
+    end
+end
+
 --=============================--
 --        Password Prompt       --
 --=============================--
